3ccb21cab545aa4b7910c80559fc681b8986787d
Tariq Khan <tariqkhan051@outlook.com>
2024-02-12 23:36:43 +0400

    add portfolio built on react

---
+++ portfolio/src/components/Testimonials.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from "react";
+import { CommandLineIcon, UsersIcon } from "@heroicons/react/24/solid";
+import { testimonials } from "../data";
+
+interface Testimonial {
+  quote: string;
+  image: string;
+  name: string;
+  company: string;
+}
+
+const Testimonials: FC = () => {
+  return (
+    <section id="testimonials">
+      <div className="container px-5 py-10 mx-auto text-center">
+        <UsersIcon className="w-10 inline-block mb-4" />
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+          Client Testimonials
+        </h1>
+
+        <div className="flex flex-wrap m-4">
+          {testimonials.map((testimonial: Testimonial) => (
+            <div className="p-4 w-full">
+              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
+                <CommandLineIcon className="block w-8 text-gray-500 mb-4" />
+
+                {/* <p className="leading-relaxed mb-6">{testimonial.quote}</p> */}
+                
+                <div className="inline-flex items-center">
+                  <img
+                    alt="testimonial"
+                    src={testimonial.image}
+                    className="flex-shrink-0 object-cover object-center"
+                  />
+                  
+                  {/* <span className="flex-grow flex flex-col pl-4">
+                    <span className="title-font font-medium text-white">
+                      {testimonial.name}
+                    </span>
+                    <span className="text-gray-500 text-sm uppercase">
+                      {testimonial.company}
+                    </span>
+                  </span> */}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
---
+++ portfolio/src/data.ts
@@ -0,0 +1,69 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "testimonials/tmp1.png",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "testimonials/tmp2.png",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "testimonials/tmp3.png",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "C# .Net",
+  "JavaScript",
+  "TypeScript",
+  "Python",
+  "Node.js",
+  "SQL",
+  "Oracle",
+  "PostgreSQL",
+];
